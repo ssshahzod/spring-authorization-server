3d707a28f5c6024025e3e5c3ce5e15ffdba26036
Cronion <ohmytarik@mail.ru>
2022-03-14 12:01:01 +0300

    authorization code

--- spring-authorization-server/samples/default-authorizationserver/src/main/java/sample/config/AuthorizationServerConfig.java
+++ spring-authorization-server/samples/default-authorizationserver/src/main/java/sample/config/AuthorizationServerConfig.java
@@ -70,15 +70,14 @@ public class AuthorizationServerConfig {
 				.clientSecret("{noop}secret")
 				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
 				//.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
 				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
 				.redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
 				//.redirectUri("http://127.0.0.1:8080/authorized")
 				.scope(OidcScopes.OPENID)
 				.scope("message.read")
 				.scope("message.write")
-				.
-				clientSettings(ClientSettings.builder().requireAuthorizationConsent(false).build())
+				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(false).build())
 				.build();
 
 
